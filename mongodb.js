f228b74906ebfe142b7391e078fe9084ed736471
Subhayu Mukherjee <subhayu.mukherjee@hubdoc.com>
2020-07-07 13:24:51 -0400

    Object IDs

--- mongodb.js
+++ mongodb.js
@@ -1,9 +1,16 @@
-const mongodb = require('mongodb');
+// const mongodb = require('mongodb');
 const config = require('./config');
-const MongoClient = mongodb.MongoClient;
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
+
+const {MongoClient, ObjectID} = require('mongodb');
 
 const connectionURL = `mongodb://${config.host}:${config.port}`;
 const DBName = config.dbName;
+
+const ID = new ObjectID();
+console.log(ID);
+console.log(ID.getTimestamp());
 
 MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
     if (error) {
@@ -13,8 +20,8 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
     const db = client.db(DBName);
 
     // db.collection('users').insertOne({
-    //     name: 'Cassandra',
-    //     age: 35
+    //     name: 'Danny',
+    //     age: 33
     // }, (error, result) => {
     //     if (error) {
     //         return console.log('Error filling document');
@@ -23,21 +30,42 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
     //     console.log(result.ops);
     // })
 
-    db.collection('users').insertMany([
-        {
-            name: 'Andrew',
-            age: 30
-        },
-        {
-            name: 'Laddu',
-            age: 36
-        }
-    ], (error, result) => {
-        if (error) {
-            return console.log('Unable to insert Document');
-        }
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Andrew',
+    //         age: 30
+    //     },
+    //     {
+    //         name: 'Laddu',
+    //         age: 36
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert Document');
+    //     }
 
-        console.log(result.ops);
-    })
+    //     console.log(result.ops);
+    // })
+
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: 'Finish Recording',
+    //         completed: true
+    //     },
+    //     {
+    //         description: 'Buy groceries',
+    //         completed: false
+    //     },
+    //     {
+    //         description: 'File Taxes',
+    //         completed: true
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert tasks');
+    //     }
+
+    //     console.log(result.ops);
+    // })
 })
 
